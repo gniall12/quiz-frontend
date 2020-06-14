6b45b3080d679d229409be574fff45b8f986db02
Niall Gallagher <gniall12@gmail.com>
2020-06-14 18:50:05 +0100

    Revert "Show participants on summary page for quizmaster"
    This reverts commit 653618d00508eefeb64bdd669cc12238ee0f876a.
    
    Changes were supposed to be committed to separate branch

--- src/app/summary/summary-qm/summary-qm.component.ts
+++ src/app/summary/summary-qm/summary-qm.component.ts
@@ -1,22 +1,17 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { BackendService } from "../../backend.service";
 import { Router } from "@angular/router";
 import { SummaryComponent } from "../summary/summary.component";
 import { environment } from "src/environments/environment";
-import { interval, Subscription, empty } from "rxjs";
-import { startWith, switchMap, catchError } from "rxjs/operators";
 
 @Component({
   selector: "app-summary-qm",
   templateUrl: "./summary-qm.component.html",
   styleUrls: ["./summary-qm.component.css"],
 })
-export class SummaryQmComponent extends SummaryComponent
-  implements OnInit, OnDestroy {
+export class SummaryQmComponent extends SummaryComponent implements OnInit {
   baseUrl: string;
   disableSubmit: boolean;
-  participantSubscription: Subscription;
-  participants: Array<object>;
 
   constructor(
     protected backendService: BackendService,
@@ -27,16 +22,6 @@ export class SummaryQmComponent extends SummaryComponent
 
   ngOnInit() {
     super.ngOnInit();
-    this.participantSubscription = interval(5000)
-      .pipe(switchMap(() => this.backendService.getParticipants()))
-      .pipe(
-        catchError(() => {
-          return empty();
-        })
-      )
-      .subscribe((res) => {
-        this.participants = res["participants"];
-      });
     this.baseUrl = environment.frontendUrl;
     this.disableSubmit = false;
   }
@@ -50,9 +35,5 @@ export class SummaryQmComponent extends SummaryComponent
 
   getQuizLink() {
     return `${this.baseUrl}?quiz-id=${this.quiz["id"]}`;
-  }
-
-  ngOnDestroy() {
-    this.participantSubscription.unsubscribe();
   }
 }
