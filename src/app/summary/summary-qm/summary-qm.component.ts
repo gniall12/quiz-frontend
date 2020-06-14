653618d00508eefeb64bdd669cc12238ee0f876a
Niall Gallagher <gniall12@gmail.com>
2020-06-14 18:44:57 +0100

    Show participants on summary page for quizmaster
    Participants endpoint is polled every 5 seconds to show
    participants for quizmaster without quizmaster having to refresh

--- src/app/summary/summary-qm/summary-qm.component.ts
+++ src/app/summary/summary-qm/summary-qm.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { BackendService } from "../../backend.service";
 import { Router } from "@angular/router";
 import { SummaryComponent } from "../summary/summary.component";
 import { environment } from "src/environments/environment";
+import { interval, Subscription, empty } from "rxjs";
+import { startWith, switchMap, catchError } from "rxjs/operators";
 
 @Component({
   selector: "app-summary-qm",
   templateUrl: "./summary-qm.component.html",
   styleUrls: ["./summary-qm.component.css"],
 })
-export class SummaryQmComponent extends SummaryComponent implements OnInit {
+export class SummaryQmComponent extends SummaryComponent
+  implements OnInit, OnDestroy {
   baseUrl: string;
   disableSubmit: boolean;
+  participantSubscription: Subscription;
+  participants: Array<object>;
 
   constructor(
     protected backendService: BackendService,
@@ -22,6 +27,16 @@ export class SummaryQmComponent extends SummaryComponent implements OnInit {
 
   ngOnInit() {
     super.ngOnInit();
+    this.participantSubscription = interval(5000)
+      .pipe(switchMap(() => this.backendService.getParticipants()))
+      .pipe(
+        catchError(() => {
+          return empty();
+        })
+      )
+      .subscribe((res) => {
+        this.participants = res["participants"];
+      });
     this.baseUrl = environment.frontendUrl;
     this.disableSubmit = false;
   }
@@ -35,5 +50,9 @@ export class SummaryQmComponent extends SummaryComponent implements OnInit {
 
   getQuizLink() {
     return `${this.baseUrl}?quiz-id=${this.quiz["id"]}`;
+  }
+
+  ngOnDestroy() {
+    this.participantSubscription.unsubscribe();
   }
 }
